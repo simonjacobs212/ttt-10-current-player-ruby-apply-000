b4e127eef848d8c1b7b21dad1265b9c32e123263
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-10-23 17:46:08 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  turn_count = 0
+  while turn_count < 9
+    turn(board, current_player)
+    turn_count += 1
+    end
+end
+
+
+def current_player(board)
+  current_player = "X"
+  if current_player == "X"
+    current_player = "O"
+  else
+    current_player = "X"
+  end
+end
